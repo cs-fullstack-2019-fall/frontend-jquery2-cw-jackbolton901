166c192e3841e8c163406ebec439deec1f1d0c33
tdude0175 <tdude0175@gmail.com>
2019-09-13 11:51:23 -0500

    Graded 11:50

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # frontend-jQuery2-cw
-
+#### You managed to achieve both problems correctly and were close on the challenge 2/2 Score: 5/5
 Use the provided HTML file to answer all of the provided JavaScript questions in the js.js file. The jQuery file is attached as well. Properly connect it to your HTML file.
--- js.js
+++ js.js
@@ -7,7 +7,7 @@ DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
 $('.hello').click(function () {
     $('.hello').css("background-color", "blue");
 });
-
+// !! You successfully changed ALL hello functions to be the same color
 /* When you mouse over the "Selectors Exercise" element, change the height&width to 20px. When you mouse off of it change it back */
 $("h1").mouseover("h1", function () {
     $("h1").css("height", "20px");
@@ -17,8 +17,12 @@ $("h1").mouseout("h1", function () {
     $("h1").css("height", "");
     $("h1").css("width", "");
 });
+// !! You successfully change the height and with of the h1 and return it to it's original position
 // CHALLENGE
 /* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
 $(".hello").click(function () {
     $(".hello").innerText.append(".");
 });
+// !! The challenge does not work. You successfully gather them but you need to just append the period to the class not the inner text.
+
+
